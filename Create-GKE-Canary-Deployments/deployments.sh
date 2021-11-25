70a8e4a748f61158ae04c820992f2295df49c120
phuongvo9 <huyphuong4116@gmail.com>
2021-11-25 17:28:56 +0700

    rollout deployment

--- Create-GKE-Canary-Deployments/deployments.sh
+++ Create-GKE-Canary-Deployments/deployments.sh
@@ -26,3 +26,28 @@ git clone https://github.com/GoogleCloudPlatform/training-data-analyst
 ln -s ~/training-data-analyst/courses/ak8s/v1.1 ~/ak8s
 cd ~/ak8s/Deployments/
 
+# create nginx-deployment.yaml
+kubectl apply -f ./nginx-deployment.yaml
+
+kubectl get deployments
+
+
+### Manually scale up and down the number of Pods in deployments
+# GUI GCP console or cmd
+kubectl scale --replicas=1 deployment nginx-deployment
+
+#
+kubectl scale --replicas=3 deployment nginx-deployment
+
+
+### Trigger a deployment rollout and a deployment rollback
+
+    # update the container image in your Deployment to nginx v1.9.1.
+kubectl set image deployment.v1.apps/nginx-deployment nginx=nginx:1.9.1 --record
+
+    # view the rollout status
+kubectl rollout status deployment.v1.apps/nginx-deployment
+        # Waiting for deployment "nginx-deployment" rollout to finish: 1 old replicas are pending termination...
+        # Waiting for deployment "nginx-deployment" rollout to finish: 1 old replicas are pending termination...
+        # deployment "nginx-deployment" successfully rolled out
+
