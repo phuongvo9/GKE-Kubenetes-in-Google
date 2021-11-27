dde91fca2e8c0b333a0f8a6afa8abaa38949294b
phuongvo9 <huyphuong4116@gmail.com>
2021-11-27 22:15:52 +0700

    Create Regional Kubernetes Engine Cluster

--- Deploy-Apps-with-Persistent-Disks/GKE-Apps-PersistentDisks.sh
+++ Deploy-Apps-with-Persistent-Disks/GKE-Apps-PersistentDisks.sh
@@ -1,4 +1,16 @@
 #  configure a highly available application by deploying WordPress using regional persistent disks on Kubernetes Engine. Regional persistent disks provide synchronous replication between two zones, which keeps your application up and running in case there is an outage or failure in a single zone. Deploying a Kubernetes Engine Cluster with regional persistent disks will make your application more stable, secure, and reliable.
+######################################################
+### Creating the Regional Kubernetes Engine Cluster
+######################################################
 
 
+CLUSTER_VERSION=$(gcloud container get-server-config --region us-west1 --format='value(validMasterVersions[0])')
 
+export CLOUDSDK_CONTAINER_USE_V1_API_CLIENT=false
+
+gcloud container clusters create repd \
+  --cluster-version=${CLUSTER_VERSION} \
+  --machine-type=n1-standard-4 \
+  --region=us-west1 \
+  --num-nodes=1 \
+  --node-locations=us-west1-a,us-west1-b,us-west1-c
