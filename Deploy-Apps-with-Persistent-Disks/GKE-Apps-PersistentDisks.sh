9afce310d22dfa47085fc8da5c516205a880b85a
phuongvo9 <huyphuong4116@gmail.com>
2021-11-27 22:14:45 +0700

    add GKE-Apps-PersistentDisks

---
+++ Deploy-Apps-with-Persistent-Disks/GKE-Apps-PersistentDisks.sh
@@ -0,0 +1,4 @@
+#  configure a highly available application by deploying WordPress using regional persistent disks on Kubernetes Engine. Regional persistent disks provide synchronous replication between two zones, which keeps your application up and running in case there is an outage or failure in a single zone. Deploying a Kubernetes Engine Cluster with regional persistent disks will make your application more stable, secure, and reliable.
+
+
+
