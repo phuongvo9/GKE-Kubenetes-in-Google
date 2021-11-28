ca9e81ec1d463546eac1dd9db98f0e9703789550
phuongvo9 <huyphuong4116@gmail.com>
2021-11-28 21:24:45 +0700

    add Configure-Network-GKE

---
+++ Configure-NetworkingPolicies-GKE/Configure-Network-GKE.sh
@@ -0,0 +1,7 @@
+# Create and test a private cluster
+
+# Configure a cluster for authorized network master access
+
+# Configure a Cluster network policy
+
+gcloud container clusters describe private-cluster --region us-central1-a
