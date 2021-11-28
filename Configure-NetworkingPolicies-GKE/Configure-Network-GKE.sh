b939ded101acfb5841bf3adb3220b996bb51db73
phuongvo9 <huyphuong4116@gmail.com>
2021-11-28 21:33:44 +0700

    Create and test a private cluster

--- Configure-NetworkingPolicies-GKE/Configure-Network-GKE.sh
+++ Configure-NetworkingPolicies-GKE/Configure-Network-GKE.sh
@@ -4,4 +4,27 @@
 
 # Configure a Cluster network policy
 
+gcloud beta container --project "qwiklabs-gcp-01-811c28398b48" clusters create "private-cluster" \
+    --zone "us-central1-a" \
+    --no-enable-basic-auth \
+    --cluster-version "1.21.5-gke.1302" \
+    --release-channel "regular" \
+    --machine-type "e2-medium" \
+    --image-type "COS_CONTAINERD" \
+    --disk-type "pd-standard" \
+    --disk-size "100" \
+    --metadata disable-legacy-endpoints=true \
+    --scopes "https://www.googleapis.com/auth/devstorage.read_only","https://www.googleapis.com/auth/logging.write","https://www.googleapis.com/auth/monitoring","https://www.googleapis.com/auth/servicecontrol","https://www.googleapis.com/auth/service.management.readonly","https://www.googleapis.com/auth/trace.append" \
+    --max-pods-per-node "110" --num-nodes "2" \
+    --logging=SYSTEM,WORKLOAD --monitoring=SYSTEM \
+    --enable-private-nodes --master-ipv4-cidr "172.16.0.0/28" \
+    --enable-ip-alias --network "projects/qwiklabs-gcp-01-811c28398b48/global/networks/default" \
+    --subnetwork "projects/qwiklabs-gcp-01-811c28398b48/regions/us-central1/subnetworks/default" \
+    --no-enable-intra-node-visibility --default-max-pods-per-node "110" \
+    --no-enable-master-authorized-networks \
+    --addons HorizontalPodAutoscaling,HttpLoadBalancing,GcePersistentDiskCsiDriver \
+    --enable-autoupgrade --enable-autorepair --max-surge-upgrade 1 --max-unavailable-upgrade 0 --enable-shielded-nodes \
+    --node-locations "us-central1-a"
+    
 gcloud container clusters describe private-cluster --region us-central1-a
+
