9df2372de6f0970c32f38b2ea628717c66d273d8
phuongvo9 <huyphuong4116@gmail.com>
2021-11-30 16:04:33 +0700

    create pods and service + test DNS

--- Create-Services-and-Ingress/create-services-ingress.sh
+++ Create-Services-and-Ingress/create-services-ingress.sh
@@ -21,3 +21,18 @@ ln -s ~/training-data-analyst/courses/ak8s/v1.1 ~/ak8s
 cd ~/ak8s/GKE_Services/
 
 kubectl apply -f dns-demo.yaml
+
+#  open an interactive session to bash running from dns-demo-1
+kubectl exec -it dns-demo-1 -- /bin/bash
+
+# install ping tool inside the pod
+apt-get update
+apt-get install -y iputils-ping
+
+ping dns-demo-2.dns-demo.default.svc.cluster.local
+    # PING dns-demo-2.dns-demo.default.svc.cluster.local (10.8.1.8) 56(84) bytes of data.
+    # 64 bytes from dns-demo-2.dns-demo.default.svc.cluster.local (10.8.1.8): icmp_seq=1 ttl=62 time=1.69 ms
+    # 64 bytes from dns-demo-2.dns-demo.default.svc.cluster.local (10.8.1.8): icmp_seq=2 ttl=62 time=0.327 ms
+#Ping the dns-demo service's FQDN, instead of a specific Pod inside the service
+ping dns-demo.default.svc.cluster.local
+
