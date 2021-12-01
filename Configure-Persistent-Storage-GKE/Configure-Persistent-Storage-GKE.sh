2303424380b1ce27199ba4351f6bfb0f5b004c1f
phuongvo9 <huyphuong4116@gmail.com>
2021-12-01 23:07:18 +0700

    Create PVs and PVCs

--- Configure-Persistent-Storage-GKE/Configure-Persistent-Storage-GKE.sh
+++ Configure-Persistent-Storage-GKE/Configure-Persistent-Storage-GKE.sh
@@ -7,3 +7,44 @@
 # Mount Google Cloud persistent disk PVCs as volumes in StatefulSets
 
 # Verify the connection of Pods in StatefulSets to particular PVs as the Pods are stopped and restarted
+
+gcloud auth list
+gcloud config list project
+
+########################################################################################################
+### Create PVs and PVCs
+########################################################################################################
+
+export my_zone=us-central1-a
+export my_cluster=standard-cluster-1
+
+source <(kubectl completion bash)
+gcloud container clusters get-credentials $my_cluster --zone $my_zone
+
+# Create and apply a manifest with a PVC
+
+    # don't need to directly configure PV objects or create Compute Engine persistent disks
+    # create a PVC, and Kubernetes automatically provisions a persistent disk
+    # create pvc-demo.yaml
+            # apiVersion: v1
+            # kind: PersistentVolumeClaim
+            # metadata:
+            # name: hello-web-disk
+            # spec:
+            # accessModes:
+            #     - ReadWriteOnce
+            # resources:
+            #     requests:
+            #     storage: 30Gi
+
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+ln -s ~/training-data-analyst/courses/ak8s/v1.1 ~/ak8s
+cd ~/ak8s/Storage/
+
+# Check PVCs
+kubectl get persistentvolumeclaim
+
+kubectl apply -f pvc-demo.yaml
+kubectl get persistentvolumeclaim
+
+
