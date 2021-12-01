4c9916b34b9cdf3a8f980e24b826ad8c343811d4
phuongvo9 <huyphuong4116@gmail.com>
2021-12-01 22:58:27 +0700

    add Configure-Persistent-Storage-GKE

---
+++ Configure-Persistent-Storage-GKE/Configure-Persistent-Storage-GKE.sh
@@ -0,0 +1,9 @@
+# Create manifests for PersistentVolumes (PVs) and PersistentVolumeClaims (PVCs) for Google Cloud persistent disks (dynamically created or existing)
+
+# Mount Google Cloud persistent disk PVCs - PersistentVolumeClaims as volumes in Pods
+
+# Use manifests to create StatefulSets
+
+# Mount Google Cloud persistent disk PVCs as volumes in StatefulSets
+
+# Verify the connection of Pods in StatefulSets to particular PVs as the Pods are stopped and restarted
