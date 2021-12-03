b3df3cef280783fe8557c2a04fdd30fcc071a63c
phuongvo9 <huyphuong4116@gmail.com>
2021-12-03 21:29:23 +0700

    add RBAC in GKE

---
+++ RBAC-in-GKE/RBAC-GKE-1.sh
@@ -0,0 +1,5 @@
+# create namespaces within a GKE cluster, and then use role-based access control to permit a non-admin user to work with Pods in a specific namespace.
+
+# Create namespaces for users to control access to cluster resources
+# Create roles and rolebindings to control access within a namespace
+
