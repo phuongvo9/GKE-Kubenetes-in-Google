299b53d498b477354951170dcf986205434c8598
phuongvo9 <huyphuong4116@gmail.com>
2021-12-04 11:08:38 +0700

    add production namespace

--- RBAC-in-GKE/RBAC-GKE-1.sh
+++ RBAC-in-GKE/RBAC-GKE-1.sh
@@ -3,3 +3,16 @@
 # Create namespaces for users to control access to cluster resources
 # Create roles and rolebindings to control access within a namespace
 
+export my_zone=us-central1-a
+export my_cluster=standard-cluster-1
+source <(kubectl completion bash)
+
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+ln -s ~/training-data-analyst/courses/ak8s/v1.1 ~/ak8s
+cd ~/ak8s/RBAC/
+
+##############################
+#Create a Namespace
+##############################
+
+
