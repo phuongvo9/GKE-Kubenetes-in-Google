2c3d66f6ee1d8c87dbb62768582993e9629b8383
phuongvo9 <huyphuong4116@gmail.com>
2021-12-04 18:43:07 +0700

    Create a Cloud SQL instance

--- Cloud SQL with Google Kubernetes Engine/CloudSQL-GKE.sh
+++ Cloud SQL with Google Kubernetes Engine/CloudSQL-GKE.sh
@@ -11,3 +11,19 @@
 
 # Install SQL Proxy as a sidecar container and use it to provide SSL access to a CloudSQL instance external to the GKE Cluster
 
+
+export my_zone=us-central1-a
+export my_cluster=standard-cluster-1
+source <(kubectl completion bash)
+
+gcloud container clusters get-credentials $my_cluster --zone $my_zone
+
+git clone https://github.com/GoogleCloudPlatform/training-data-analyst
+ln -s ~/training-data-analyst/courses/ak8s/v1.1 ~/ak8s
+cd ~/ak8s/Cloud_SQL/
+
+# Create a Cloud SQL instance
+gcloud sql instances create sql-instance --tier=db-n1-standard-2 --region=us-central1
+
+
+export SQL_NAME=[Cloud SQL Instance Name]
